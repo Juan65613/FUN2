364320f7b2f2a3497a855a9ab3f830d722c762a2
FAMILIA <FAMILIA@DESKTOP-D2POBE1>
2024-03-25 17:38:48 -0500

    uptated

---
+++ ESC/src/fun4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class fun4 {
+	 public static void main(String[] args)
+	    {
+	
+	        Scanner sc = new Scanner("seller,products,SELLS");
+
+	        String name = sc.nextLine();
+
+	        char id = sc.next().charAt(0);
+
+	        int sells = sc.nextInt();
+	        int product = sc.nextInt();
+	       
+	        System.out.println("id: ");
+	        System.out.println("sells: ");
+	        System.out.println("product: ");
+	    }
+	}
+
+
